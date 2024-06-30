4075f579e2cb4528d5a96327b55977e2a39de016
Anindita Acharjee <aninditaacharjee8@gmail.com>
2024-06-30 15:18:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+def calculator():
+ 
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    operation = input("Enter the operation (+, -, *, /): ")
+
+    
+    if operation == "+":
+        result = num1 + num2
+    elif operation == "-":
+        result = num1 - num2
+    elif operation == "*":
+        result = num1 * num2
+    elif operation == "/":
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            print("Error: Division by zero is not allowed.")
+            return
+    else:
+        print("Error: Invalid operation. Please try again.")
+        return
+
+
+    print("The result is:", result)
+
+calculator()
